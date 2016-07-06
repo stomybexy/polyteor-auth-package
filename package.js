ca03b7b004f5514add46ce49cb637d95d441b8ef
Jonatan Sombié <jonatan.sombie@gmail.com>
2016-07-06 13:41:40 +0000

    Update version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jonatan:polyteor-auth',
-  version: '0.0.3',
+  version: '0.0.4',
   // Brief, one-line summary of the package.
   summary: 'Polymer element wrapping Meteor.user function.',
   // URL to the Git repository containing the source code for this package.
